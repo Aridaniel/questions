e89177bf848f1ba500f382adba2da3431943b952
Aridaniel <aridagnars@gmail.com>
2020-10-01 23:45:32 +0000

    Loka push

--- main.js
+++ main.js
@@ -6,3 +6,16 @@ $('#myModal').on('shown.bs.modal', function () {
   {
       alert("Not enough times")
   }
+  function carouselTakki1()
+  {
+      alert("Wrong he is just Zen")
+  }
+  function carouselTakki2()
+  {
+      alert("You are right, just look at him!!!")
+  }
+  function carouselTakki3()
+  {
+      alert("He is pretty close though")
+  }
+  
